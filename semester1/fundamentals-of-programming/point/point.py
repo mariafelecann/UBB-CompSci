c1e1975be70f32e239a4f5f99010a01716ab4208
Maria Felecan <159628578+mariafelecann@users.noreply.github.com>
2024-03-02 13:34:52 +0200

    Add files via upload

---
+++ semester1/fundamentals-of-programming/point/point.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_x(self):
+        return self.__x
+
+    def set_x(self, x):
+        self.__x = x
+
+    def get_y(self):
+        return self.__y
+
+    def set_y(self, y):
+        self.__y = y
