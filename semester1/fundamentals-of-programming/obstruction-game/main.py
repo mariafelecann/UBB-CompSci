8250053c798a3aa1bb9ca63ead007cc27d6fe43e
Maria Felecan <159628578+mariafelecann@users.noreply.github.com>
2024-03-04 20:06:53 +0200

    Update main.py

--- semester1/fundamentals-of-programming/obstruction-game/main.py
+++ semester1/fundamentals-of-programming/obstruction-game/main.py
@@ -1,11 +1,10 @@
 from board.board import Board
 from ai.ai import AI
 from console.console import Console
-from gui.gui import GUI
+
 
 game = AI(Board())
 
 ui = Console(game)
 ui.start()
 
-#ui = GUI(game)
