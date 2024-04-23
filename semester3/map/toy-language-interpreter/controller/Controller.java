face60230fa0a7bb251319082ae2d7fb3cfdc75a
Maria Felecan <159628578+mariafelecann@users.noreply.github.com>
2024-04-23 21:13:05 +0300

    Update Controller.java

--- semester3/map/toy-language-interpreter/controller/Controller.java
+++ semester3/map/toy-language-interpreter/controller/Controller.java
@@ -24,28 +24,7 @@ public class Controller {
 
     public IRepository getRepository(){return repo;}
 
-    /*
-
-    public void allStep() throws MyException, IOException {
-        List<ProgramState> states_from_repo = repo.getListOfProgramStates();
-        ProgramState prg = states_from_repo.get(0);
-        while (!prg.getExeStack().isEmpty()) {
-            System.out.println(prg.toString());
-            repo.savePrgStateinLOG(prg);
-            try {
-                prg.oneStep();
-                prg.getHeap().setContent(prg.unsafeGarbageCollector(getAddrFromSymbTable(prg.getSymTable().getContent().values()), getAddrFromSymbTable(prg.getHeap().getContent().values()), prg.getHeap().getContent()));
-            } catch (MyException ex) {
-                System.out.println(ex.getMessage());
-            }
-
-        }
-
-        System.out.println(prg.toString());
-        repo.savePrgStateinLOG(prg);
-    }
-
-     */
+   
 
     public List<Integer> getAddrFromSymbTable(Collection<Value> symbTableValues) {
         return symbTableValues.stream().filter(v -> v instanceof RefValue)
@@ -66,22 +45,18 @@ public class Controller {
 
     public void allStep() throws InterruptedException {
         executor = Executors.newFixedThreadPool(2);
-        //remove the completed programs
+        
         List<ProgramState> prgList= removeCompletedProgram(repo.getListOfProgramStates());
         List<ProgramState> states_from_repo = repo.getListOfProgramStates();
         ProgramState prg = states_from_repo.get(0);
         while(prgList.size() > 0){
             prg.getHeap().setContent(prg.unsafeGarbageCollector(getAddrFromSymbTable(prg.getSymTable().getContent().values()), getAddrFromSymbTable(prg.getHeap().getContent().values()), prg.getHeap().getContent()));
             oneStepForAllProgramStates(prgList);
-            //remove the completed programs
+           
             prgList=removeCompletedProgram(repo.getListOfProgramStates());
         }
         executor.shutdownNow();
-        //HERE the com.example.toylanguagegui.repository still contains at least one Completed Prg
-        // and its List<PrgState> is not empty. Note that oneStepForAllPrg calls the method
-        //setPrgList of com.example.toylanguagegui.repository in order to change the com.example.toylanguagegui.repository
-
-        // update the com.example.toylanguagegui.repository state
+        
         repo.setProgramStateList(prgList);
     }
 
@@ -91,7 +66,7 @@ public class Controller {
         if (executor == null) {
             executor = Executors.newFixedThreadPool(2);
         }
-        //before the execution, print the PrgState List into the log file
+    
         prgList.forEach(prg -> {
             try {
                 System.out.println(prg.toString());
@@ -116,7 +91,7 @@ public class Controller {
                     return null;
                 }).filter(p -> p!=null)
                             .collect(Collectors.toList());
-                    //add the new created threads to the list of existing threads
+                    
                     prgList.addAll(newPrgList);
 
                     prgList.forEach(prg -> {
