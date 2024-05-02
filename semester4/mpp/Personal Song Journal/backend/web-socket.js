dbfe4920515d6aa4e0faad2e67474a06d5a08efe
Maria Felecan <159628578+mariafelecann@users.noreply.github.com>
2024-05-02 17:21:49 +0300

    updated back with 2 db 1:n entities

---
+++ semester4/mpp/Personal Song Journal/backend/web-socket.js
@@ -0,0 +1,59 @@
+const http = require("http");
+const WebSocket = require("ws");
+const { GenreRepository } = require("./repository/genre_repository");
+const SongRepository = require("./repository/song_repository").SongRepository;
+
+const port = 4000;
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+const songRepository = new SongRepository();
+
+wss.on("connection", (ws) => {
+  console.log("new websocket client connected");
+
+  ws.on("close", () => {
+    console.log("websocket client disconnected");
+  });
+});
+
+// setInterval(() => {
+//   var new_songs = songRepository.generateSongs();
+//   new_songs.forEach((song) => {
+//     songRepository.add(song);
+//   });
+//   const songs = songRepository.getAllSongs();
+//   wss.clients.forEach((client) => {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(JSON.stringify(songs));
+//     }
+//   });
+// }, 20000);
+
+// server.listen(port, () => {
+//   console.log(`websocket server running on port ${port}`);
+// });
+
+setInterval(async () => {
+  try {
+    const new_songs = await songRepository.generateSongs();
+    if (Array.isArray(new_songs)) {
+      for (const song of new_songs) {
+        await songRepository.add(song);
+      }
+
+      const songs = await songRepository.getAllSongs();
+
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(songs));
+        }
+      });
+    } else {
+      console.error("Generated songs is not an array:", new_songs);
+    }
+  } catch (error) {
+    console.error("Error processing new songs:", error);
+  }
+}, 20000);
